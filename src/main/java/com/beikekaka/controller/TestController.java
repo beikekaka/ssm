138d72cdaee2f89280eb2b4cfc086dba79d52b6c
beikekaka <beikekaka@gmail.com>
2018-07-27 21:57:08 +0800

    TestController修改 master

--- src/main/java/com/beikekaka/controller/TestController.java
+++ src/main/java/com/beikekaka/controller/TestController.java
@@ -1,7 +1,26 @@
 package com.beikekaka.controller;
 
+import com.beikekaka.model.User;
+import com.beikekaka.service.IUserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.annotation.Resource;
+
 /**
  * Created by Issac on 2018/5/28.
  */
+@Controller
 public class TestController {
+    @Resource
+    private IUserService userService;
+
+    @RequestMapping("/showTest")
+    public ModelAndView showUser(){
+        ModelAndView mav = new ModelAndView("index");
+        User user = this.userService.selectUser(2);
+        mav.addObject("user",user);
+        return mav;
+    }
 }
